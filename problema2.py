03e9fb02d3cb445ea5c44f6e801a5927a68b8ba1
Diego Ardais <diego.ardais@edu.pucrs.br>
2024-04-08 19:51:05 -0300

    fix problema 2

--- problema2.py
+++ problema2.py
@@ -1,40 +1,56 @@
 from threading import Semaphore, Thread
 
 # parâmetros de entrada
-NUM_SAVAGE = 5
-NUM_SERVING = 10
+N = 5 # numero de canibais
+M = 10  # número de porções que o cozinheiro adiciona à travessa
 
 # Semáforos e variáveis
+cozinha = Semaphore(0)  
+enchendo = Semaphore(0)
+comida = Semaphore(1)
 mutex = Semaphore(1)
-full_pot = Semaphore(0)
-empty_pot = Semaphore(0)
-servings = 0
+count = 1
 
 # cozinheiro
 def cook():
+    global count
     while True:
-        empty_pot.acquire()
-        full_pot.release()
-        print("Cozinheiro enche o pote")
+        cozinha.acquire() # wait cozinha
+        enche_travessa()
+        for _ in range(M):
+            comida.release()
+        count = M
+        enchendo.release()
 
 # canibal
-def savage(servings_size: int, servings):
+def savage():
+    global count
     while True:
+        comida.acquire()
         mutex.acquire()
-        if servings == 0:
-            empty_pot.release()
-            full_pot.acquire()
-            servings = servings_size
-        servings -= 1
-        print("Canibal come")
+        count -= 1
+        if count == 0:
+            cozinha.release() # signal
+            enchendo.acquire()
         mutex.release()
+        come()
+
+# Função para simular o ato de comer
+def come():
+    print("Canibal come")
+
+# Função fictícia para encher a travessa
+def enche_travessa():
+    print("Cozinheiro enche o pote")
 
 # Função para iniciar o jantar dos canibais
 def dining_savages():
     cook_thread = Thread(target=cook)
 
-    for i in range(NUM_SAVAGE):
-        savage_thread = Thread(target=savage, args=(NUM_SERVING, servings))
+    savage_threads = []
+    for _ in range(N):
+        savage_thread = Thread(target=savage)
+        savage_threads.append(savage_thread)
         savage_thread.start()
 
     cook_thread.start()
